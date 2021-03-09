677321b85c1b1b4655c96600203321952e926e5b
luis-chamorro <luis.a.chamorro.21@dartmouth.edu>
2021-03-08 23:42:31 -0500

    Added error handling when a gas station is not found

--- directions.js
+++ directions.js
@@ -8,8 +8,8 @@ const metersPerMile = 1609.344;
 
 /* Round to three decimal places */
 const roundThree = (num) => {
-  return (Math.round(num * 1000) / 1000);
-}
+  return Math.round(num * 1000) / 1000;
+};
 
 const directionsResponse = async (req, res) => {
   let startId = req.params.start,
@@ -57,6 +57,8 @@ const directionsResponse = async (req, res) => {
         let firstStop = true; // meaning we should account for how much gas is left initially
         let step, stepDist;
 
+        let lastStopIndex = 0;
+
         // keep track of place in polyline
         let polyIndex = 0;
 
@@ -66,16 +68,22 @@ const directionsResponse = async (req, res) => {
 
           // handle stopping in the middle of a step longer than full tank distance
           // also handle the case where fuel will run out but tank is more than 30% full
-          if ((stepDist > initDist * metersPerMile && firstStop) ||
-              stepDist > fullTankDist * metersPerMile ||
-              (distSinceStop + stepDist >= initDist * metersPerMile &&
-                distSinceStop / initDist > 0.3 &&
-                (firstStop ||
-                  (distSinceStop + stepDist >= fullTankDist * metersPerMile &&
-                  distSinceStop / fullTankDist > 0.3)))) {            
+          if (
+            (stepDist > initDist * metersPerMile && firstStop) ||
+            stepDist > fullTankDist * metersPerMile ||
+            (distSinceStop + stepDist >= initDist * metersPerMile &&
+              distSinceStop / initDist > 0.3 &&
+              (firstStop ||
+                (distSinceStop + stepDist >= fullTankDist * metersPerMile &&
+                  distSinceStop / fullTankDist > 0.3)))
+          ) {
             // for indexing future points on this step
-            while (roundThree(coords[polyIndex].latitude) != roundThree(step.start_location.lat) ||
-            roundThree(coords[polyIndex].longitude) != roundThree(step.start_location.lng)) {
+            while (
+              roundThree(coords[polyIndex].latitude) !=
+                roundThree(step.start_location.lat) ||
+              roundThree(coords[polyIndex].longitude) !=
+                roundThree(step.start_location.lng)
+            ) {
               polyIndex++;
             }
             let startPolyIndex = polyIndex;
@@ -84,9 +92,13 @@ const directionsResponse = async (req, res) => {
             // stepDistLeft tends to be a bit larger than stepDist
             let stepDistLeft = 0;
             let pathDists = [];
-            while (stepDistLeft < stepDist &&
-              (roundThree(coords[polyIndex].latitude) != roundThree(step.end_location.lat) ||
-              roundThree(coords[polyIndex].longitude) != roundThree(step.end_location.lng))) {
+            while (
+              stepDistLeft < stepDist &&
+              (roundThree(coords[polyIndex].latitude) !=
+                roundThree(step.end_location.lat) ||
+                roundThree(coords[polyIndex].longitude) !=
+                  roundThree(step.end_location.lng))
+            ) {
               let startLatLng = coords[polyIndex];
               let endLatLng = coords[polyIndex + 1];
 
@@ -98,14 +110,18 @@ const directionsResponse = async (req, res) => {
 
             // loop path coordinates until the step can be completed without stopping
             let k = 0;
-            while (stepDistLeft > (initDist * metersPerMile - distSinceStop) &&
-              (firstStop || (stepDistLeft > (fullTankDist * metersPerMile - distSinceStop)))) {
-
+            while (
+              stepDistLeft > initDist * metersPerMile - distSinceStop &&
+              (firstStop ||
+                stepDistLeft > fullTankDist * metersPerMile - distSinceStop)
+            ) {
               distSinceStop += pathDists[k];
               stepDistLeft -= pathDists[k];
 
-              if (distSinceStop >= initDist * metersPerMile &&
-                (firstStop || (distSinceStop >= fullTankDist * metersPerMile))) {
+              if (
+                distSinceStop >= initDist * metersPerMile &&
+                (firstStop || distSinceStop >= fullTankDist * metersPerMile)
+              ) {
                 // should stop before this section of the path, so look for a stop near the beginning of the section
                 // find one gas station within 15000 meters (about 9 mi) of this point
                 let nearStop = await nearestStops(
@@ -135,22 +151,18 @@ const directionsResponse = async (req, res) => {
                   firstStop = false;
                   stopsList.push(thisStop);
                   distSinceStop = pathDists[k];
-
+                  lastStopIndex = i;
                 } else {
                   console.log(nearStop[0]); // error?
-                  k = Math.min((k + 99), pathDists.length); // temporary? causes slight inaccuracy bc this dist isn't added
+                  k = Math.min(k + 99, pathDists.length); // temporary? causes slight inaccuracy bc this dist isn't added
                 }
-
-              } 
+              }
               k++;
             }
 
             // account for distance traveled since the last stop on this stretch
             distSinceStop += pathDists.slice(k).reduce((x, y) => x + y);
-          }
-          
-
-          else {
+          } else {
             distSinceStop += stepDist;
             if (distSinceStop >= initDist * metersPerMile) {
               if (firstStop || distSinceStop >= fullTankDist * metersPerMile) {
@@ -181,8 +193,21 @@ const directionsResponse = async (req, res) => {
                   }
                   firstStop = false;
                   stopsList.push(thisStop);
+                  lastStopIndex = i;
                 } else {
                   console.log(nearStop[0]); // error?
+
+                  // Backtrack multiple steps until a gas station is found
+                  let j = i - 1;
+                  while (j > lastStopIndex + 1 && nearStop[0] == undefined) {
+                    nearStop = await nearestStops(
+                      legs[0].steps[j - 1].end_location.lat,
+                      legs[0].steps[j - 1].end_location.lng,
+                      searchRadius,
+                      1
+                    );
+                    j = j - 1;
+                  }
                 }
 
                 distSinceStop = stepDist;
@@ -195,6 +220,7 @@ const directionsResponse = async (req, res) => {
       else {
         stops = parseInt(req.params.numStops);
         const longToLat = 53 / 69.172;
+        const latToMeters = 110567;
 
         // Calculate where the stops are
 
@@ -225,15 +251,25 @@ const directionsResponse = async (req, res) => {
         // Search for stops near the points that were found
         for (var i = 0; i < stopSearchList.length; i++) {
           let currStop = stopSearchList[i];
-          let nearStop = await nearestStops(
-            currStop.latitude,
-            currStop.longitude,
-            searchRadius,
-            1
-          );
 
-          if (nearStop[0] != undefined &&
-              nearStop[0].geometry != undefined) {
+          const multIncrementer = 1.5;
+          let currMult = 1;
+          let nearStop = [];
+
+          while (
+            nearStop[0] == undefined &&
+            searchRadius * currMult < (goalDist * latToMeters) / 2
+          ) {
+            nearStop = await nearestStops(
+              currStop.latitude,
+              currStop.longitude,
+              searchRadius * currMult,
+              1
+            );
+            currMult *= multIncrementer;
+          }
+
+          if (nearStop[0] != undefined && nearStop[0].geometry != undefined) {
             let loc = nearStop[0].geometry.location;
             let thisStop = {
               latitude: loc.lat,
@@ -249,7 +285,9 @@ const directionsResponse = async (req, res) => {
 
             stopsList.push(thisStop);
           } else {
-            console.log(nearStop[0]); // error?
+            res.status(500).send({ message: "No gas stations found" });
+            console.log("No gas stations found");
+            return;
           }
         }
       }
